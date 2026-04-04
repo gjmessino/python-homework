cb7fc378a52af7deeee357ec743f6160ce748af2
gjmessino <gabyjmessino@gmail.com>
2026-04-03 20:15:19 -0700

    fixed some errors

--- assignment1/assignment1.py
+++ assignment1/assignment1.py
@@ -1,63 +1,87 @@
 # Task 1: Hello
-def greeting():
+def hello():
     return "Hello!"
 # Task 2: Greet with a Formatted String
-def greeting2(name):
-    return "Hello, " + name
+def greet(name):
+    return "Hello, " + name + "!"
 # Task 3: Calculator
 def calc (num1, num2, operator = "multiply"):
     try:
         num1 = int(num1)
         num2 = int(num2)
+    except ValueError:
+        print("Pick a real number")
+        return
     if operator == "add":
         return num1+num2
     elif operator == "subtract":
         return num1-num2
     elif operator == "divide":
-        return num1/num2
+        try:
+            return num1/num2
+        except ZeroDivisionError:
+            print("These numbers cannot be divided")
     elif operator == "modulo":
         return num1%num2
     elif operator == "int_divide":
-        return num1/num2
+        try:
+            return num1/num2
+        except ZeroDivisionError:
+            print("These numbers cannot be divided")
     else:
-        return num1*num2
+        return int(num1/num2)
 # Task 4: Data Type Conversion
 def data_type_conversion(val,name):
     if name == "str":
-        return str(val)
+        try:
+            new_data = str(val)
+            return new_data
+        except TypeError:
+            print("Type Error")
     elif name == "float":
-        return float(val)
+        try:
+            new_data = float(val)
+            return new_data
+        except TypeError:
+            print("Type Error")
     elif name == "int":
-        return int(val)
+        try:
+            new_data = int(val)
+            return new_data
+        except TypeError:
+            print("Type Error")
     else:
-        print("Improper value given")
         return
 # Task 5: Grading System, Using *args
 def grade(*args):
-    ave = sum(args) / len(args)
-    if ave >= 90:
-        return "A"
-    elif ave>= 80:
-        return "B"
-    elif ave>= 70:
-        return "C"
-    elif ave>= 60:
-        return "D"
-    else:
-        return "F"
+    try:
+        ave = sum(args) / len(args)
+        if ave >= 90:
+            return "A"
+        elif ave>= 80:
+            return "B"
+        elif ave>= 70:
+            return "C"
+        elif ave>= 60:
+            return "D"
+        else:
+            return "F"
+    except ZeroDivisionError:
+        print ("Could not be divided")
 # Task 6: Use a For Loop with a Range
 def repeat (word, count):
+    new_word = ""
     for i in range(count):
-        word = word + word
-    return word
+        new_word += word
+    return new_word
 # Task 7: Student Scores, Using **kwargs
 def student_scores(posit, **kwargs):
     if posit == "mean":
-        return sum(kwargs.values) / len(kwargs)
+        return sum(kwargs.values()) / len(kwargs)
     elif posit == "best":
         top_score = 0
         name = ""
-        for k, v in kwargs.items:
+        for k, v in kwargs.items():
             if v > top_score:
                 top_score = v
                 name = k
@@ -65,7 +89,7 @@ def student_scores(posit, **kwargs):
 #Task 8: Titleize, with String and List Operations
 def titleize(title):
     new_title = title.split()
-    new_title = new_title.title()
+    new_title = new_title.capitalize()
     for word in new_title:
         if word == "a" or "on" or "an" or "the" or "of" or "and" or "is" or "in":
             word.lower()
@@ -81,7 +105,7 @@ def hangman(secret, guess):
                 new_secret += "_"
     return new_secret
 # Task 10: Pig Latin, Another String Manipulation Exercise
-def igpay_atinlay(phrase):
+def pig_latin(phrase):
     phrase.split()
     new_phrase = ""
     for words in phrase:
@@ -92,9 +116,10 @@ def igpay_atinlay(phrase):
             words.append("qu")
             words.append("ay")
         else:
-            first = words [0]
-            words.slice(1)
-            words.append(first)
+            while (words[0] != "a" or "e" or "i" or "o" or "u"):
+                first = words [0]
+                words.slice(1)
+                words.append(first)
             words.append("ay")
         new_phrase += words
     return new_phrase

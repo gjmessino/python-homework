e7d4fc7a014362a356c2b3f43d94f191178046c7
gjmessino <gabyjmessino@gmail.com>
2026-04-08 17:48:13 -0700

    started task 2

--- assignment2/assignment2.py
+++ assignment2/assignment2.py
@@ -11,4 +11,25 @@ def diary():
                     file.close()
     except Exception as e:
         print(f"An exception has occured. {e}")
+        return
+
 # Task 2: Read a CSV File
+import csv
+
+def read_employees():
+    try:
+        my_dict = {}
+        my_list = []
+        with open('../csv/employees.csv', 'r') as file:
+            reader = csv.reader(file)
+            my_dict['field'] = next(reader)
+            for row in reader:
+                my_list.append(row)
+            my_dict['rows'] = my_list
+        return my_dict
+    except Exception as e:
+        print(f"An exception has occured. {e}")
+        return
+
+employees = read_employees()
+print(employees)

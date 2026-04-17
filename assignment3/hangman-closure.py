3dc6a181004db6998de04eb7edaa6a34d1395664
gjmessino <gabyjmessino@gmail.com>
2026-04-17 15:36:18 -0700

    finished task 4

--- assignment3/hangman-closure.py
+++ assignment3/hangman-closure.py
@@ -1,16 +1,22 @@
 def make_hangman(secret_word):
     guesses = []
-    hang = str("_" * len(secret_word))
+    not_guessed = True
+    hang_list = list("_" * len(secret_word))
     def hangman_closure(letter):
         nonlocal guesses
+        nonlocal not_guessed
+        nonlocal hang_list
         guesses.append(letter)
-        if letter in secret_word:
-            hang[secret_word.find(letter)] = letter
-        print(hang)
-        if guesses in secret_word:
+        indices = [ind for ind, character in enumerate(secret_word) if character == letter]
+        for item in indices:
+            hang_list[item] = letter
+        print(str(hang_list))
+        if "".join(hang_list) == secret_word:
+            not_guessed = False
             return True
         else:
             return False
-    return hangman_closure(hangman_closure(input("Guess a letter: ")))
+    while not_guessed == True:
+        hangman_closure(input("Guess a letter: "))
 
 make_hangman(input("Pick a secret word: "))
--- assignment3/log-decorator.py
+++ assignment3/log-decorator.py
@@ -1,5 +1,19 @@
-def logger_decorator():
+import logging
+
+def logger_decorator(*args, **kwargs):
+    logger = logging.getLogger(__name__ + "_parameter_log")
+    logger.setLevel(logging.INFO)
+    logger.addHandler(logging.FileHandler("./decorator.log","a"))
+    logger.log(logging.INFO, "this string would be logged")
 
 @logger_decorator
-def my_function():
-    print("Hello, world!")
+def hello_world():
+    print("Hello, World!")
+
+@logger_decorator
+def my_second_function(*args, **kwargs):
+    return logger_decorator()
+
+logger_decorator()
+hello_world()
+my_second_function()

378d51669b35a751e171de18cd21a44889bf805a
gjmessino <gabyjmessino@gmail.com>
2026-04-20 19:46:00 -0700

    changed hangman

--- assignment3/hangman-closure.py
+++ assignment3/hangman-closure.py
@@ -1,23 +1,20 @@
 def make_hangman(secret_word):
-    guesses = []
-    not_guessed = True
-    hang_list = list("_" * len(secret_word))
-    new_letter = input("Guess a letter: ")
     def hangman_closure(letter):
-        nonlocal guesses
-        nonlocal not_guessed
-        nonlocal hang_list
+        guesses = []
+        hang_list = list("_" * len(secret_word))
         guesses.append(letter)
+        print("".join(hang_list))
         indices = [ind for ind, character in enumerate(secret_word) if character == letter]
         for item in indices:
             hang_list[item] = letter
-        print(str(hang_list))
         if "".join(hang_list) == secret_word:
-            not_guessed = False
             return True
         else:
             return False
-    return hangman_closure(new_letter)
+    return hangman_closure()
 
-secret = input("Pick a secret word: ")
-make_hangman(secret)
+secret1 = input("Pick a secret word: ")
+x = True
+while x == True:
+    guessed_let = input("Guess a letter ")
+    secret1(guessed_let)

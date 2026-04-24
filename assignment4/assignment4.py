51233487826b9f367b0939d3149c065d184e0398
gjmessino <gabyjmessino@gmail.com>
2026-04-24 13:42:03 -0700

    changed file paths and deleted old ones

--- assignment4/assignment4.py
+++ assignment4/assignment4.py
@@ -1,62 +1,63 @@
 import pandas as pd
-# # Task 1: Introduction to Pandas - Creating and Manipulating DataFrames
-# data = {'Name': ['Alice', 'Bob', 'Charlie'],
-#            'Age': [25, 30, 35],
-#            'City': ['New York', 'Los Angeles', 'Chicago']}
 
-# task1_data_frame = pd.DataFrame(data)
-# task1_with_salary = task1_data_frame.copy()
-# print('Original Data')
-# print(task1_data_frame)
+# Task 1: Introduction to Pandas - Creating and Manipulating DataFrames
+data = {'Name': ['Alice', 'Bob', 'Charlie'],
+           'Age': [25, 30, 35],
+           'City': ['New York', 'Los Angeles', 'Chicago']}
 
-# task1_with_salary['Salary'] = [70000, 80000, 90000]
-# print('Add Salary')
-# print(task1_with_salary)
+task1_data_frame = pd.DataFrame(data)
+task1_with_salary = task1_data_frame.copy()
+print('Original Data')
+print(task1_data_frame)
 
-# task1_older = task1_with_salary.copy()
-# task1_older['Age'] +=1
-# print('Incriment Age')
-# print(task1_older)
+task1_with_salary['Salary'] = [70000, 80000, 90000]
+print('Add Salary')
+print(task1_with_salary)
 
-# task1_older.to_csv('employees.csv', index=False)
+task1_older = task1_with_salary.copy()
+task1_older['Age'] +=1
+print('Incriment Age')
+print(task1_older)
 
-# # Task 2: Loading Data from CSV and JSON
-# task2_employees = pd.read_csv('employees.csv')
-# print('Reprinting Data from Task 1')
-# print(task2_employees)
+task1_older.to_csv('assignment4/employees.csv', index=False)
 
-# task2_data = ({'Name' : ['Eve', 'Frank'],
-#                'Age' : ['28', '40'],
-#                'City' : ['Miami', 'Seatle'],
-#                'Salary' : ['60000', '95000']})
-# json_employees = pd.DataFrame(task2_data)
-# json_employees.to_json('additional_employees.json', index = False)
+# Task 2: Loading Data from CSV and JSON
+task2_employees = pd.read_csv('employees.csv')
+print('Reprinting Data from Task 1')
+print(task2_employees)
 
-# more_employees = pd.concat ([task2_employees, json_employees], ignore_index=True)
-# print('Task 2 Data')
-# print(more_employees)
+task2_data = ({'Name' : ['Eve', 'Frank'],
+               'Age' : ['28', '40'],
+               'City' : ['Miami', 'Seatle'],
+               'Salary' : ['60000', '95000']})
+json_employees = pd.DataFrame(task2_data)
+json_employees.to_json('assignment4/additional_employees.json', index = False)
 
-# # Task 3: Data Inspection - Using Head, Tail, and Info Methods
-# print('Head')
-# more_employees.head()
+more_employees = pd.concat ([task2_employees, json_employees], ignore_index=True)
+print('Task 2 Data')
+print(more_employees)
 
-# first_three = more_employees.iloc[0:3]
-# print('First Three')
-# print(first_three)
+# Task 3: Data Inspection - Using Head, Tail, and Info Methods
+print('Head')
+more_employees.head()
 
-# print('Tail')
-# print(more_employees.tail())
+first_three = more_employees.iloc[0:3]
+print('First Three')
+print(first_three)
 
-# print('Last Two')
-# last_two = more_employees.iloc[3:]
-# print(last_two)
+print('Tail')
+print(more_employees.tail())
 
-# employee_shape = more_employees.shape
-# print('Shape')
-# print(employee_shape)
+print('Last Two')
+last_two = more_employees.iloc[3:]
+print(last_two)
 
-# print('Info')
-# more_employees.info()
+employee_shape = more_employees.shape
+print('Shape')
+print(employee_shape)
+
+print('Info')
+more_employees.info()
 
 # Task 4: Data Cleaning
 dirty_data = pd.read_csv('assignment4/dirty_data.csv')

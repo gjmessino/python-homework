620d638cef9de290d7edda9ca569a1c9e5e51d2f
gjmessino <gabyjmessino@gmail.com>
2026-05-25 17:29:37 -0700

    fixed typos and bugs

--- assignment8/get_books.py
+++ assignment8/get_books.py
@@ -22,22 +22,21 @@ print(len(li_lists))
 results = []
 for entry in li_lists:
     title = entry.find_element(By.CSS_SELECTOR, 'h3.cp-title')
-    author_list = entry.find_elements(By.CSS_SELECTOR, 'a.author-link')
-    if type(author_list) is list:
-        print(author_list.text)
-    else:
-        author = author_list.text
-    print(author)
+    author_list = []
+    author_entry = entry.find_elements(By.CSS_SELECTOR, 'a.author-link')
+    for auth in author_entry:
+        author_list.append(auth.text)
+    author = " ;".join(author_list)
     year = entry.find_element(By.CSS_SELECTOR, 'span.display-info-primary')
-    print(year.text)
     dict_entry = {'Title' : title.text,
-                  'Author' : author.text,
+                  'Author' : author,
                   'Format-Year': year.text}
     results.append(dict_entry)
 
-df = pd.DataFame(results)
+df = pd.DataFrame(results)
+print(df)
 
-df.to_csv('../get_books.csv')
-df.to_json('../get_books.json')
+df.to_csv('./get_books.csv')
+df.to_json('./get_books.json')
 
 driver.quit()

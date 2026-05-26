a5a9275a15f0711aa6b6dd3a672666f91d2e4f10
gjmessino <gabyjmessino@gmail.com>
2026-05-25 17:01:39 -0700

    restarted tasks 1-4

--- assignment8/get_books.py
+++ assignment8/get_books.py
@@ -1,16 +1,14 @@
-# Imports
-import pandas as pd
 import json
+import csv
+import pandas as pd
 from time import sleep
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 
-#Creating Driver
 driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
 
-# Letting driver get webpage
 try:
     driver.get('https://durhamcounty.bibliocommons.com/v2/search?query=learning%20spanish&searchType=smart')
     sleep(2) # wait 2 seconds
@@ -18,27 +16,26 @@ try:
 except Exception as e:
     print(f"An exception occurred: {type(e).__name__} {e}")
 
-#Task 3
-book_list = driver.find_elements(By.CSS_SELECTOR, 'li.cp-search-results-item')
+li_lists = driver.find_elements(By.CSS_SELECTOR, 'li.cp-search-result-item')
+print(len(li_lists))
+
 results = []
+for entry in li_lists:
+    title = entry.find_element(By.CSS_SELECTOR, 'h3.cp-title')
+    author_list = entry.find_elements(By.CSS_SELECTOR, 'a.author-link')
+    if type(author_list) is list:
+        print(author_list.text)
+    else:
+        author = author_list.text
+    print(author)
+    year = entry.find_element(By.CSS_SELECTOR, 'span.display-info-primary')
+    print(year.text)
+    dict_entry = {'Title' : title.text,
+                  'Author' : author.text,
+                  'Format-Year': year.text}
+    results.append(dict_entry)
 
-#iterate through books
-if (book_list):
-    for book in book_list:
-        title = book.find_element(By.CSS_SELECTOR, 'h3[class= cp-title]')
-        print(title)
-        author = book.find_element(By.CSS_SELECTOR, 'a[class= author-link]')
-        info_div = book.find_element(By.CSS_SELECTOR, 'div[class= cp-format-info]')
-        info_text = info_div.find_element(By.CSS_SELECTOR, 'span [class = display-info-primary]')
-        dict_entry = {'Title' : title,
-                      'Author' : author,
-                      'Format-Year': info_text}
-        results.append(dict_entry)
+df = pd.DataFame(results)
 
-df = pd.DataFrame(results)
-
-#Task 4
-df.to_csv('./get_books.csv')
-df.to_json('get_books.json', orient='records', indent=2)
-
-driver.quit()
+df.to_csv('../get_books.csv')
+df.to_json('../get_books.json')

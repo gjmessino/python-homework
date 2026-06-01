d3b9404b397739c5a9608fbac15b0320b008feb7
Gaby <145401032+gjmessino@users.noreply.github.com>
2026-06-01 11:42:13 -0700

    Update sql_intro_2.py

--- assignment9/sql_intro_2.py
+++ assignment9/sql_intro_2.py
@@ -7,4 +7,9 @@ try:
     with  sqlite3.connect("../db/lesson.db") as conn: 
         cursor = conn.cursor()
 except sqlite3.Error as e:
-    print(f"An error occurred: {e}")
+    print(f"An error occurred: 
+
+with sqlite3.connect("../db/lesson.db") as conn:
+    sql_statement = ""SELECT * FROM lesson"""
+    df = pd.read_sql_query(sql_statement, conn)
+    print(df)

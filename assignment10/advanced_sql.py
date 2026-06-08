275ba72b6b9fa7dc2b0531c6ed5f01b5fd88dbf3
Gaby <145401032+gjmessino@users.noreply.github.com>
2026-06-08 16:01:40 -0700

    started tasks 3-4

--- assignment10/advanced_sql.py
+++ assignment10/advanced_sql.py
@@ -19,7 +19,7 @@ try:
 
 ## Task 2
         sql_statement2 =("""
-                        SELECT c.customer_id,
+                        SELECT c.customer_name,
                         AVG(sub.total_price) as total_average_price
                         FROM customers as c
                         LEFT JOIN(
@@ -28,25 +28,56 @@ try:
                          FROM orders o
                          JOIN line_items l ON o.order_id = l.order_id
                          JOIN products p ON l.product_id = p.product_id
-                         GROUP BY o.order_id)
-                        AS  sub ON c.customer_id = sub.customer_id_b
+                         )
+                        AS sub ON customer_id = sub.customer_id_b
+                        GROUP BY customer_id 
                         """)
         cursor.execute(sql_statement2)
         results = cursor.fetchall()
         for row in results:
             print(results)
 
-## Task 3
-        sql_statement3 = ("""
-                          SELECT c.customer_id 
-                          FROM customers c
-                          WHERE c.customer_name = 'Perez and Sons'
-                          SELECT e.employee_id 
-                          FROM Employees e
-                          WHERE e.first_name = 'Miranda' AND e.last_name = 'Harris'
-                          SELECT p.product_id
-                          HAVING MIN(p.price) LIMIT 5
-""")
-        
-        cursor.execute("INSERT INTO orders (order_id, customer_id, employee_id) VALUES ()")
-    print(f'An error occurred: {e}')
+# ## Task 3
+#         order = []
+#         sql = ("""SELECT c.customer_id 
+#                FROM customers c
+#                WHERE c.customer_name = 'Perez and Sons'""")
+#         cursor.execute(sql)
+#         row = cursor.fetchall()
+#         cust_id = row[0]
+
+#         sql2 = ("""SELECT e.employee_id 
+#                 FROM Employees e
+#                 WHERE e.first_name = 'Miranda' AND e.last_name = 'Harris'""")
+#         cursor.execute(sql2)
+#         row = cursor.fetchall()
+#         emp_id = row[0]
+
+#         sql3= ("""SELECT p.product_id
+#                From products p
+#                ORDER BY p.price ASC
+#                LIMIT 5""")
+#         cursor.execute(sql3)
+#         rows = cursor.fetchall()
+#         order_ids = []
+#         for row in rows:
+#              sql4 = ("""INSERT INTO line_items (product_id,quantity) VALUES (row,5)
+#                      RETURNING order_id""")
+#              cursor.execute(sql4)
+#              order = cursor.fetchall()
+#              order_ids.append(order)
+#         sql5 = ("""INSERT INTO orders (order_id,customer_id,emplooyee_id,date) VALUES (order_ids, cust_id, emp_id, '06/08/2026' )""")
+
+## Task 4
+        sql_statement3 = ("""SELECT e.first_name, e.last_name,
+                         COUNT(o.employee_id) as order_count
+                         FROM employees e                         
+                         WHERE order_count >= 5
+                         JOIN orders o ON e.employee_id=o.employee_id
+                         """)
+        cursor.execute(sql_statement3)
+        results = cursor.fetchall()
+        print(results)
+
+except Exception as e:
+        print(f'An error occurred: {e}')

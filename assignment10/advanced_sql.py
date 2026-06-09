a1ef4fb12d3c9f62e745452b38e5886d038d47ef
Gaby <145401032+gjmessino@users.noreply.github.com>
2026-06-09 10:05:27 -0700

    fixed bugs in tasks 3-4

--- assignment10/advanced_sql.py
+++ assignment10/advanced_sql.py
@@ -28,9 +28,10 @@ try:
                          FROM orders o
                          JOIN line_items l ON o.order_id = l.order_id
                          JOIN products p ON l.product_id = p.product_id
+                         GROUP BY o.customer_id
                          )
-                        AS sub ON customer_id = sub.customer_id_b
-                        GROUP BY c.customer_id
+                        AS sub ON c.customer_id = sub.customer_id_b
+                        GROUP BY c.customer_id, c.customer_name; 
                         """)
         cursor.execute(sql_statement2)
         results = cursor.fetchall()
@@ -43,15 +44,13 @@ try:
                FROM customers c
                WHERE c.customer_name = 'Perez and Sons'""")
         cursor.execute(sql)
-        row = cursor.fetchone()[0]
-        cust_id = row[0]
+        cust_id = cursor.fetchone()[0]
 
         sql2 = ("""SELECT e.employee_id 
                 FROM Employees e
                 WHERE e.first_name = 'Miranda' AND e.last_name = 'Harris'""")
         cursor.execute(sql2)
-        row = cursor.fetchone()[0]
-        emp_id = row[0]
+        emp_id = cursor.fetchone()[0]
 
         sql3= ("""SELECT p.product_id
                From products p
@@ -59,27 +58,43 @@ try:
                LIMIT 5""")
         cursor.execute(sql3)
         rows = cursor.fetchall()
+
+        conn.execute('PRAGMA foreign_keys = 1')
         sql4 = ("""INSERT INTO orders (customer_id, employee_id, date) VALUES (?, ?, '2026-06-08') RETURNING order_id""")
         cursor.execute(sql4, (cust_id, emp_id))
         order_id = cursor.fetchone()[0]
 
+        conn.execute('PRAGMA foreign_keys = 1')
         sql5 = ("""INSERT INTO line_items (order_id, product_id, quantity) VALUES (?, ?, 10)""")
         for row in rows:
              prod_id = row[0]
              cursor.execute(sql5, (order_id, prod_id))
+        
+        sql6 = ("""
+                SELECT l.line_item_id, p.product_name
+                FROM line_items l
+                JOIN products p ON l.product_id = p.product_id
+                ORDER BY p.price ASC
+                LIMIT 5
+                """)
+        cursor.execute(sql6)
+        results = cursor.fetchall()
+        print(results)
 
-## Task 4
-        sql_statement3 = ("""SELECT e.first_name, e.last_name, e.employee_id, o.order_id
-                         FROM employees e      
-                         JOIN orders o ON e.employee_id=o.employee_id
-                         GROUP BY e.employee_id, e.first_name, e.last_name                   
-                         HAVING COUNT(o.order_id) > 5
+# Task 4
+        sql_statement3 = ("""
+                          SELECT e.first_name, e.last_name,
+                          COUNT(o.order_id) AS order_count
+                          FROM employees e      
+                          JOIN orders o ON e.employee_id=o.employee_id
+                          GROUP BY e.employee_id, e.first_name, e.last_name                  
+                          HAVING COUNT(o.order_id) > 5
                          """)
         cursor.execute(sql_statement3)
         results = cursor.fetchall()
         print(results)
 
-        conn.commit()
+#         conn.commit()
 except Exception as e:
         conn.rollback()
         print(f'An error occurred: {e}')
